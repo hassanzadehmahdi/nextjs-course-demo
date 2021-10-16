9131522aa6169846f108ab203faa1ca7e970e48a
mahdihassanzade <mahdi139460@gmail.com>
2021-10-16 13:13:24 +0330

    fixed fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -33,7 +33,7 @@ export const getStaticPaths = async () => {
   client.close();
 
   return {
-    fallback: false,
+    fallback: "blocking",
     paths: meetups.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
